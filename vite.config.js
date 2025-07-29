97fe5fbef57d1a532940955a310e71bc9212da09
AnnisaRizki30 <annisarizkililiandari@gmail.com>
2025-07-30 06:46:51 +0700

    push update code

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
-  base: './', // 🔥 Important untuk static hosting
+  plugins: [tailwindcss()],
+  base: './', 
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
